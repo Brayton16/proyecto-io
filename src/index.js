8adfc778a73d72c3cdf2c3cda015f0ae54cbbba1
Brayton16 <Brayton2011@hotmail.es>
2024-10-18 20:56:40 -0600

    Main page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Proyecto1 from './proyecto_1/Proyecto1';
+import Proyecto2 from './proyecto_2/Proyecto2';
+import Proyecto3 from './proyecto_3/Proyecto3';
+import Proyecto4 from './proyecto_4/Proyecto4';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App/>} />
+      <Route path="/proyecto_1" element={<Proyecto1/>} />
+      <Route path="/proyecto_2" element={<Proyecto2/>} />
+      <Route path="/proyecto_3" element={<Proyecto3/>} />
+      <Route path="/proyecto_4" element={<Proyecto4/>} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/proyecto_0/Proyecto0.jsx
+++ src/proyecto_0/Proyecto0.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+
 
 const Proyecto0  = () => {
+    const navigate = useNavigate();
+
     return(
         <div className='m-5'>
-            <p>Hola Mundo!</p>
+            <div className='text-center'><h1>Proyecto Investigación de Operaciones</h1></div>
+            <div className='text-center'><h3>Creado por: Brayton Solano, Julián Madrigal, Deylan Sandoval, Álex Brenes</h3></div>
+            <div className='text-center m-5'><p>Por favor ingrese a un algorimo para continuar</p></div>
+            <div className='d-flex'>
+                <div className='container text-center card-deck'>
+                    <div className="card bg-light border border-danger" style={{height: 448.850}}>
+                        <img src="/floyd.gif" className="card-img-top" alt="" style={{height: 235.262}}/>
+                        <div className="card-body" style={{height: 181.600}}>
+                            <h5 className="card-title" style={{height: 48}} >Rutas Óptimas (Algoritmo de Floyd)</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button onClick={() => navigate('/proyecto_1')} className="btn btn-outline-dark">Ir al algoritmo</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='container text-center'>
+                    <div className="card bg-light border border-danger" style={{height: 448.850}}>
+                        <img src="/knapsack.png" className="card-img-top" alt="..." style={{height: 235.262}}/>
+                        <div className="card-body" style={{height: 181.600}}>
+                            <h5 className="card-title" style={{height: 48}}>Problema de la mochila</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button onClick={() => navigate('/proyecto_2')} className="btn btn-outline-dark">Ir al algoritmo</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='container text-center'>
+                    <div className="card bg-light border border-danger" style={{height: 448.850}}>
+                        <img src="/sports.jpg" className="card-img-top" alt="..." style={{height: 235.262}}/>
+                        <div className="card-body" style={{height: 181.600}}>
+                            <h5 className="card-title" style={{height: 48}}>Series deportivas</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button onClick={() => navigate('/proyecto_3')} className="btn btn-outline-dark">Ir al algoritmo</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='container text-center'>
+                    <div className="card bg-light border border-danger" style={{height: 448.850}}>
+                        <img src="/tree.jpg" className="card-img-top" alt="" style={{height: 235.262}}/>
+                        <div className="card-body" style={{height: 181.600}}>
+                            <h5 className="card-title" style={{height: 48}}>Árboles binarios de búsqueda óptimos</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button onClick={() => navigate('/proyecto_4')} className="btn btn-outline-dark">Ir al algoritmo</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
